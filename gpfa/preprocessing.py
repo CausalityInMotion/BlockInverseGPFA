9f5379b974484d76ef82d4a60caeee1eda3df876
bmusangu <brooks.musangu@gmail.com>
2023-05-09 11:08:00 -0400

    updated docstring

--- gpfa/preprocessing.py
+++ gpfa/preprocessing.py
@@ -1,8 +1,15 @@
-"""
-GPFA preprocessing Class for neural data.
+# ...
+# Copyright 2021 Brooks M. Musangu and Jan Drugowitsch.
+# license Modified BSD, see LICENSE.txt for details.
+# ...
 
-:copyright: Copyright 2021 Brooks M. Musangu and Jan Drugowitsch.
-:license: Modified BSD, see LICENSE.txt for details.
+"""
+GPFA preprocessing class for neural data.
+
+.. autosummary::
+    :toctree: _toctree/gpfa
+
+    EventTimesToCounts
 """
 
 import sklearn
@@ -12,58 +19,124 @@ import numpy as np
 class EventTimesToCounts(sklearn.base.TransformerMixin):
     """
     This class bins spike trains of time points and provides
-    a method to covernt `neo.SpikeTrains` data to a matrix
-    with counted time points
-
-    This below serves as notes for when we update the
-    docstring, please ignore them
-    ===========================================================
-    # If `t_stop` is not specified by the user it is
-    # handled in two ways depending on the data type of `X`. If `X`
-    # is a `neo.SpikeTrain` object, `t_stop` is set by getting it
-    # from `X[0].t_stop.magnitude` since it is assumed that `t_stop`
-    # is the same across all neurons. However, if `X` is not a
-    # `neo.SpikeTrain` object then `t_stop` is obtained by finding
-    # the largest spike time across all neurons
-
-    # we add a small fraction of the `bin_size` to `t_stop` to
-    # ensure that the last bin is included whenever `t_stop` falls
-    # on the boundary. Furthermore, this helps to include spikes
-    # that fall be in the last bin.
-
-    # If `True`, check if `t_stop` falls on the boundary of the last
-    # bin. If it indeed falls on the boundary, then
-    # `extrapolate_last_bin` is set to `False` for this particular
-    # trial since extrapolating the last bin is only required when
-    # `t_stop` falls within the last bin
-    =============================================================
+    methods that control if the last bin includes ``t_stop``
+    and covernts ``neo.SpikeTrains`` data to a matrix with counted
+    time points. The user has the option to specify ``t_stop``,
+    but if it is not specified, it is handled in two ways depending
+    on the data type of ``X``. If ``X`` is a ``neo.SpikeTrain``
+    object, ``t_stop`` is set to ``X[0].t_stop.magnitude`` since it
+    is assumed that ``t_stop`` is the same across all neurons.
+    Otherwise, ``t_stop`` is obtained by finding the largest spike
+    time across all neurons. To extrapolate the last bin, a small
+    fraction of the ``bin_size`` is added to the ``t_stop`` to ensure
+    that it is included within the last bin.
 
     Parameters
     ----------
-    bin_size : float
+    bin_size : float, default : 0.02 [s]
         Data bin width in [s] e.g., 0.02, 0.05, 0.1, 1, etc.
-        Default : 0.02 [s]
-    t_stop : float
+
+    t_stop : float, optional, default : None
         The stop time of the trial. If not specified, it is
-        retrieved from the `t_stop` attribute of `neo.Spiketrain`
+        retrieved from the ``t_stop`` attribute of ``neo.Spiketrain``
         or defaults to the largest spike time across all neurons.
-        Default : None
-    extrapolate_last_bin : boolean
+
+    extrapolate_last_bin : boolean, optional, default : False
         Controls if the last bin includes t_stop.
-        If `False`, the last bin is the one before t_stop (or includes
-        t_stop if it matches the bin's final edge). Then, all events
-        after the bin's final edge are ignored. In this case, X_out
-        contains integer event counts.
-        If 'True', the last bin includes t_stop, and the event counts
-        are extrapolated to take into account that t_stop is smaller
-        than the last bin's final edge. In this case, X_out contains
-        float event counts which, for the final bin, might be
-        non-integer values.
-        Default : False
+        If ``False``, the last bin is the one before ``t_stop`` (or
+        includes ``t_stop`` if it matches the bin's final edge). Then,
+        all events after the bin's final edge are ignored. In this
+        case, ``X_out`` contains integer event counts.
+
+        If ``True``, the last bin includes ``t_stop``, and the event
+        counts are extrapolated to take into account that ``t_stop`` is
+        smaller than the last bin's final edge. In this case, ``X_out``
+        contains ``float`` event counts which, for the final bin, might
+        be ``non-integer`` values.
+
 
     Method
     ------
     transform
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from gpfa.preprocessing import EventTimesToCounts
+    >>> try:
+    >>>     import neo
+    >>>     neo_imported = True
+    >>> except ImportError:
+    >>>     print('neo failed to import. Skipping neo-specific examples.')
+    >>>     neo_imported = False
+    >>> bin_size = 0.1  # [s]
+    >>> t_stop = 0.8  # [s]
+    >>> X = [
+    ...     [0, 0.1, 0.15, 0.4, 0.5, 0.6, 0.8],
+    ...     [0.05, 0.3, 0.4, 0.55, 0.7]
+    ...     ]
+    >>> ettc = EventTimesToCounts(
+    ...                           bin_size=bin_size,
+    ...                           t_stop=t_stop,
+    ...                           extrapolate_last_bin=False
+    ...                          )
+    >>> ettc.transform(X)
+    array([[1, 2, 0, 0, 1, 2, 0, 1],
+           [1, 0, 1, 0, 1, 1, 1, 0]])
+
+    >>> ettc_extrapolate_last_bin = EventTimesToCounts(
+    ...                             bin_size=bin_size,
+    ...                             t_stop=t_stop,
+    ...                             extrapolate_last_bin=True
+    ...                          )
+    >>> ettc.transform(X)
+    array([[1, 2, 0, 0, 1, 2, 0, 1],
+           [1, 0, 1, 0, 1, 1, 1, 0]])
+
+    >>> # Here t_stop is set to None (the default value)
+    >>> ettc = EventTimesToCounts(
+    ...                           bin_size=bin_size,
+    ...                           t_stop=None,
+    ...                           extrapolate_last_bin=False
+    ...                          )
+    >>> ettc.transform(X)
+    array([[1, 2, 0, 0, 1, 2, 0, 1],
+           [1, 0, 1, 0, 1, 1, 1, 0]])
+
+    >>> t_stop2 = 0.88  # [s]
+    >>> ettc = EventTimesToCounts(
+    ...                           bin_size=bin_size,
+    ...                           t_stop=t_stop2,
+    ...                           extrapolate_last_bin=False
+    ...                          )
+    >>> ettc.transform(X)
+    array([[1, 2, 0, 0, 1, 2, 0, 1],
+           [1, 0, 1, 0, 1, 1, 1, 0]])
+
+    >>> t_stop2 = 0.88  # [s]
+    >>> ettc_extrapolate_last_bin = EventTimesToCounts(
+    ...                             bin_size=bin_size,
+    ...                             t_stop=t_stop2,
+    ...                             extrapolate_last_bin=True
+    ...                          )
+    >>> ettc.transform(X)
+    array([[1.  , 2.  , 0.  , 0.  , 1.  , 2.  , 0.  , 0.  , 1.25],
+           [1.  , 0.  , 1.  , 0.  , 1.  , 1.  , 1.  , 0.  , 0.  ]])
+
+    >>> t_stop = 0.8  # [s]
+    >>> if neo_imported:
+    >>>     neoSpikeTrain = [
+    ...         neo.SpikeTrain(X[0],units='sec', t_stop=t_stop),
+    ...         neo.SpikeTrain(X[1], units='sec', t_stop=t_stop)
+    ...         ]
+    >>> ettc = EventTimesToCounts(
+                        bin_size=bin_size,
+                        t_stop=None,
+                        extrapolate_last_bin=False
+                        )
+    >>> ettc.transform(neoSpikeTrain)
+    array([[1, 2, 0, 0, 1, 2, 0, 1],
+           [1, 0, 1, 0, 1, 1, 1, 0]])
 
     """
     def __init__(self, bin_size=0.02, t_stop=None,
@@ -80,8 +153,8 @@ class EventTimesToCounts(sklearn.base.TransformerMixin):
         ---------
         X : numpy.array or neo.SpikeTrain
             An array-like of observation sequences from one trial.
-            Each element in `X` can be of different lengths but the
-            trial duration (i.e., `t_stop`) is the same across all
+            Each element in ``X`` can be of different lengths but the
+            trial duration (i.e., ``t_stop``) is the same across all
             neuron.
 
         Returns
